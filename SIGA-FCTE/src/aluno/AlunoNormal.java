5ef17bcda68729e092d9d391e5d36ed79eb3a776
PAPerfilTeste <110242063+PArthur006@users.noreply.github.com>
2025-05-06 14:32:16 -0300

    Implementação da classe AlunoNormal com regra padrão de matrícula
    Criada a classe AlunoNormal estendendo Aluno.
    Construtor chama a superclasse para inicializar os dados básicos.
    Método podeMatricular sempre retorna true, permitindo matrícula normal.

--- SIGA-FCTE/src/aluno/AlunoNormal.java
+++ SIGA-FCTE/src/aluno/AlunoNormal.java
@@ -1,5 +1,16 @@
 package aluno;
 
-public class AlunoNormal {
+public class AlunoNormal extends Aluno {
+    public AlunoNormal(String nome, String matricula, String curso){
+        super(nome, matricula, curso);
+    }
 
+    @Override
+    public boolean podeMatricular(String disciplina){
+        return true; // Preciso acrescentar os pré-requisitos!
+    }
+
+    public boolean recebeNotas(){
+        return true;
+    }
 }
