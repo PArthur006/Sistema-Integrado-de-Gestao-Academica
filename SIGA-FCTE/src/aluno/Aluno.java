b2f3ccc60d0944f7dbc8f741ac132339cb2f4672
PAPerfilTeste <110242063+PArthur006@users.noreply.github.com>
2025-05-06 14:29:58 -0300

    Criação da classe abstrata Aluno com atributos básicos e método abstrato de matrícula
    Implementada a classe abstrata Aluno contendo nome, matrícula, curso e lista de disciplinas.
    Adicionados os métodos getters, setters e toString().
    Incluído o método abstrato podeMatricular(String disciplina), que será implementado pelas subclasses.

--- SIGA-FCTE/src/aluno/Aluno.java
+++ SIGA-FCTE/src/aluno/Aluno.java
@@ -1,5 +1,65 @@
 package aluno;
 
-public class Aluno {
+import java.util.List;
+
+public abstract class Aluno {
+    protected String nome;
+    protected String matricula;
+    protected String curso;
+    protected List<String> disciplinasMatriculadas;
+
+    // MÉTODO CONSTRUTOR
+    public Aluno(String nome, String matricula, String curso){
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public String getMatricula(){
+        return matricula;
+    }
+
+    public abstract boolean podeMatricular(String disciplina);
+
+    public void matricular(String disciplina){
+        if(podeMatricular(disciplina)){
+            disciplinasMatriculadas.add(disciplina);
+        }
+    }
+
+    public void trancarDisciplina(String disciplina){
+        disciplinasMatriculadas.remove(disciplina);
+    }
+
+    public void trancarSemestre(){
+        disciplinasMatriculadas.clear();
+    }
+
+    // GETTERS E SETTERS
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public List<String> getDisciplinasMatriculadas() {
+        return disciplinasMatriculadas;
+    }
 
 }
+
