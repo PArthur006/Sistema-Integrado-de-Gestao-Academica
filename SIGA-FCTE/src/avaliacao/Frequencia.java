872a83481cc93d50cfb5cc39529d3cd8cb5c9b6a
PArthur006 <parthur.rodrigues06@gmail.com>
2025-07-26 22:16:08 -0300

    Adiciona arquivos de avaliações e frequências, implementa métodos para ler e salvar dados

--- SIGA-FCTE/src/avaliacao/Frequencia.java
+++ SIGA-FCTE/src/avaliacao/Frequencia.java
@@ -1,15 +1,42 @@
 package avaliacao;
 
 public class Frequencia {
+    private String turma;
+    private String aluno;
+    private int faltas;
     private int totalAulas;
     private int presencas;
 
+    // Construtor para persistência
+    public Frequencia(String turma, String aluno, int faltas) {
+        this.turma = turma;
+        this.aluno = aluno;
+        this.faltas = faltas;
+    }
+
+    // Construtor antigo para uso interno (mantido para compatibilidade)
     public Frequencia(int totalAulas, int presencas) {
         this.totalAulas = totalAulas;
         this.presencas = presencas;
     }
 
+    public String getTurma() {
+        return turma;
+    }
+
+    public String getAluno() {
+        return aluno;
+    }
+
+    public int getFaltas() {
+        return faltas;
+    }
+
     public double calcularFrequencia() {
-        return (double) presencas / totalAulas * 100;
+        if (totalAulas > 0) {
+            return (double) presencas / totalAulas * 100;
+        } else {
+            return 0.0;
+        }
     }
 }
--- SIGA-FCTE/src/avaliacao/Nota.java
+++ SIGA-FCTE/src/avaliacao/Nota.java
@@ -1,37 +1,47 @@
 package avaliacao;
 
 public class Nota {
-    private double p1;
-    private double p2;
-    private double p3;
-    private double l;
-    private double s;
+    private String turma;
+    private String aluno;
+    private double[] notas;
 
+    // Construtor para persistência
+    public Nota(String turma, String aluno, double[] notas) {
+        this.turma = turma;
+        this.aluno = aluno;
+        this.notas = notas;
+    }
+
+    // Construtor antigo para uso interno (mantido para compatibilidade)
     public Nota(double p1, double p2, double p3, double l, double s) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.p3 = p3;
-        this.l = l;
-        this.s = s;
+        this.notas = new double[]{p1, p2, p3, l, s};
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public String getAluno() {
+        return aluno;
+    }
+
+    public double[] getNotas() {
+        return notas;
     }
 
     public double getP1() {
-        return p1;
+        return notas.length > 0 ? notas[0] : 0.0;
     }
-
     public double getP2() {
-        return p2;
+        return notas.length > 1 ? notas[1] : 0.0;
     }
-
     public double getP3() {
-        return p3;
+        return notas.length > 2 ? notas[2] : 0.0;
     }
-
     public double getL() {
-        return l;
+        return notas.length > 3 ? notas[3] : 0.0;
     }
-
     public double getS() {
-        return s;
+        return notas.length > 4 ? notas[4] : 0.0;
     }
 }
